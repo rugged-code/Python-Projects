5f06579823eb51da347d50bb6873ed680af4db47
Shrey Raj <sr.shrey.8283@gmail.com>
2026-05-19 01:36:35 +0530

    Created the non OOPS version of the game (only the starting program)

---
+++ Games/SnakeGame/main.py
@@ -0,0 +1,33 @@
+from turtle import Turtle, Screen
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=600, height=600)
+screen.title("My Snake Game")
+screen.tracer(0)
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+segments = []
+
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments) -1, 0, -1):
+        x_cor = segments[seg_num - 1].xcor()
+        y_cor = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(x_cor, y_cor)
+    segments[0].forward(20)
+
+
+screen.exitonclick()
