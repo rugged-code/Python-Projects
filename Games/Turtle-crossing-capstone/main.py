ea48cb660384116bc74cf8ccf994fe185d86a38d
Shrey Raj <sr.shrey.8283@gmail.com>
2026-05-20 19:01:15 +0530

    Add the game files

---
+++ Games/Turtle-crossing-capstone/main.py
@@ -0,0 +1,41 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_cars()
+    car_manager.move_cars()
+
+    # collision with the cars
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_score()
+        scoreboard.update_scoreboard()
+
+
+
+
+
+screen.exitonclick()
